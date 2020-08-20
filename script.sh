9468fb3192a7dc63e688ec3d022f3586e6eaf4ce
akhilraj939 <65364447+akhilraj939@users.noreply.github.com>
2020-08-19 20:10:09 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y ansible
 sudo apt-get install -y git
-git clone 
-cd Project
+git clone https://github.com/akhilraj939/Learnings.git
+cd Learnings/Project
 
 sudo ansible-playbook updateplay.yml
